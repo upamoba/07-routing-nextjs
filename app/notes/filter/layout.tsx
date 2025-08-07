d5e42ebdc66d55d42ee6f56cf74e00c204cebea6
uty <fgmapam@gmail.com>
2025-08-07 22:54:58 +0300

    Update layout.tsx

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { PropsWithChildren, ReactNode } from 'react';
-import SidebarNotes from './@sidebar/SidebarNotes'
+import { 
+  // PropsWithChildren, 
+  ReactNode } from 'react';
 import styles from './FilterLayout.module.css';
 
 type FilterLayoutProps = {
 children: ReactNode
-  sidebar?: ReactNode;
+  sidebar: ReactNode;
 };
 
 export default function FilterLayout({ children, sidebar }: FilterLayoutProps) {
