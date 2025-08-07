54237338c791c3a15f4a47b05ba8da2202ca2c32
uty <fgmapam@gmail.com>
2025-08-07 23:49:11 +0300

    Update layout.tsx

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,14 +1,29 @@
-import React, { PropsWithChildren } from 'react';
+// import React, { PropsWithChildren } from 'react';
+import type { ReactNode } from 'react'
 import styles from './NotesLayout.module.css';
 
+// export default function NotesLayout({
+//   children,
+//   preview,
+// }: PropsWithChildren<{ preview?: React.ReactNode }>) {
+//   return (
+//     <div className={styles.wrapper}>
+//       <div className={styles.listArea}>{children}</div>
+//       {preview && <div className={styles.previewContainer}>{preview}</div>}
+//     </div>
+//   );
+// }
 export default function NotesLayout({
   children,
   preview,
-}: PropsWithChildren<{ preview?: React.ReactNode }>) {
+}: {
+  children: ReactNode
+  preview: ReactNode
+}) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.listArea}>{children}</div>
       {preview && <div className={styles.previewContainer}>{preview}</div>}
     </div>
-  );
+  )
 }
