a0944892d93875bb70dcb22e834e3a2c781c5aa2
uty <fgmapam@gmail.com>
2025-08-08 00:02:39 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,26 +5,7 @@ import {  ReactNode } from 'react';
 import TanStackProvider from '../components/TanStackProvider/TanStackProvider';
 
 
-// export default function RootLayout({
-//   children,
-//   modal,
-// }: {
-//   children: ReactNode
-//   modal: ReactNode
-// }) {
-//   return (
-//     <html lang="uk">
-//       <body suppressHydrationWarning>
-//         <TanStackProvider>
-//           <Header />
-//           {children}
-//           <Footer />
-//           {modal}
-//         </TanStackProvider>
-//       </body>
-//     </html>
-//   )
-// }
+
 interface RootLayoutProps {
   children: ReactNode
   modal: ReactNode
