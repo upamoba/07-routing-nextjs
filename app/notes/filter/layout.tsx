cecfdab14da7855114f8e9df86c88e6cb5342e82
uty <fgmapam@gmail.com>
2025-08-07 22:50:21 +0300

    Update layout.tsx

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { PropsWithChildren, ReactNode } from 'react';
+import SidebarNotes from './@sidebar/SidebarNotes'
 import styles from './FilterLayout.module.css';
 
 type FilterLayoutProps = {
-
+children: ReactNode
   sidebar?: ReactNode;
 };
 
-export default function FilterLayout({ children, sidebar }: PropsWithChildren<FilterLayoutProps>) {
+export default function FilterLayout({ children, sidebar }: FilterLayoutProps) {
    return (
     <div className={styles.container}>
       <aside className={styles.sidebar}>{sidebar}</aside>
