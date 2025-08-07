46aee935b491b5d18ffb9c826a091a780d2a39a7
uty <fgmapam@gmail.com>
2025-08-07 22:10:14 +0300

    Update page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,62 +1,55 @@
 
-
-// interface NoteDetailsPageProps {
-//   params: { id: string };
-// }
-
-// export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
-//   const noteId = params.id;
-//   const queryClient = new QueryClient();
-
-
-//   await queryClient.prefetchQuery({
-//     queryKey: ['note', noteId],
-//     queryFn: () => fetchNoteById(noteId),
-//   });
-
- 
-//   const dehydratedState = dehydrate(queryClient);
-
-//   return (
-//     <QueryClientProvider client={queryClient}>
-     
-//       <HydrationBoundary state={dehydratedState}>
-//         <NoteDetailsClient noteId={noteId} />
-//       </HydrationBoundary>
-//     </QueryClientProvider>
-//   );
-// }
-
 import React from 'react'
 import {
   QueryClient,
-  QueryClientProvider,
+  // QueryClientProvider,
   dehydrate,
   HydrationBoundary
 } from '@tanstack/react-query'
 import { fetchNoteById } from '@/lib/api'
 import NoteDetailsClient from './NoteDetails.client'
 
-export default async function NoteDetailsPage({
-  params,
-}: {
-  params: { id: string }
-}) {
-  const noteId = params.id
-  const queryClient = new QueryClient()
+// export default async function NoteDetailsPage({
+//   params,
+// }: {
+//   params: { id: string }
+// }) {
+//   const noteId = params.id
+//   const queryClient = new QueryClient()
+
+//   await queryClient.prefetchQuery({
+//     queryKey: ['note', noteId],
+//     queryFn: () => fetchNoteById(noteId),
+//   })
+
+//   const dehydratedState = dehydrate(queryClient)
+
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <HydrationBoundary state={dehydratedState}>
+//         <NoteDetailsClient noteId={noteId} />
+//       </HydrationBoundary>
+//     </QueryClientProvider>
+//   )
+//  }
+ type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function NoteDetailsPage({ params }: PageProps) {
+  const { id: noteId } = await params;
+  const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ['note', noteId],
     queryFn: () => fetchNoteById(noteId),
-  })
+  });
 
-  const dehydratedState = dehydrate(queryClient)
+  const dehydratedState = dehydrate(queryClient);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <HydrationBoundary state={dehydratedState}>
-        <NoteDetailsClient noteId={noteId} />
-      </HydrationBoundary>
-    </QueryClientProvider>
-  )
+    <HydrationBoundary state={dehydratedState}>
+      <NoteDetailsClient noteId={noteId} />
+    </HydrationBoundary>
+  );
 }
