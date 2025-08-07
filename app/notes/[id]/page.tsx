aade920aa9fd866af6d00e35c7b9ef955fc19a82
uty <fgmapam@gmail.com>
2025-08-07 22:05:08 +0300

    Update page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,31 +1,62 @@
-import React from 'react'; 
-import { fetchNoteById } from '@/lib/api';  
-import { QueryClient,QueryClientProvider, dehydrate, HydrationBoundary } from '@tanstack/react-query'
-import NoteDetailsClient from './NoteDetails.client';
 
-interface NoteDetailsPageProps {
-  params: { id: string };
-}
 
-export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
-  const noteId = params.id;
-  const queryClient = new QueryClient();
+// interface NoteDetailsPageProps {
+//   params: { id: string };
+// }
 
+// export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
+//   const noteId = params.id;
+//   const queryClient = new QueryClient();
+
+
+//   await queryClient.prefetchQuery({
+//     queryKey: ['note', noteId],
+//     queryFn: () => fetchNoteById(noteId),
+//   });
+
+ 
+//   const dehydratedState = dehydrate(queryClient);
+
+//   return (
+//     <QueryClientProvider client={queryClient}>
+     
+//       <HydrationBoundary state={dehydratedState}>
+//         <NoteDetailsClient noteId={noteId} />
+//       </HydrationBoundary>
+//     </QueryClientProvider>
+//   );
+// }
+
+import React from 'react'
+import {
+  QueryClient,
+  QueryClientProvider,
+  dehydrate,
+  HydrationBoundary
+} from '@tanstack/react-query'
+import { fetchNoteById } from '@/lib/api'
+import NoteDetailsClient from './NoteDetails.client'
+
+export default async function NoteDetailsPage({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const noteId = params.id
+  const queryClient = new QueryClient()
 
   await queryClient.prefetchQuery({
     queryKey: ['note', noteId],
     queryFn: () => fetchNoteById(noteId),
-  });
+  })
 
- 
-  const dehydratedState = dehydrate(queryClient);
+  const dehydratedState = dehydrate(queryClient)
 
   return (
     <QueryClientProvider client={queryClient}>
-     
       <HydrationBoundary state={dehydratedState}>
         <NoteDetailsClient noteId={noteId} />
       </HydrationBoundary>
     </QueryClientProvider>
-  );
+  )
 }
